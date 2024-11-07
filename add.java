3f5433882a6406e64e49187418df2b5d6f741731
Nagashreer6515 <mfsi.nagashreer@gmail.com>
2024-11-07 15:23:42 +0530

    conflictFile1

--- add.java
+++
@@ -1,8 +0,0 @@
-class HelloWorld {
-    public static void main(String[] args) {
-
-        System.out.println("Hello new change");
-        System.out.println("Try programiz.pro");
-    }
-}
-
---
+++ p1.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public int add(){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println(add());
+    }
+}
