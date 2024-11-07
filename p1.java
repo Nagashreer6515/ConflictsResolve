ca3e325f4bcd196d33cea8348ce03bf94007caa8
Nagashreer6515 <mfsi.nagashreer@gmail.com>
2024-11-07 15:26:55 +0530

    conflictFile2

---
+++ p1.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public void multiply(){
+        System.out.println(a*b);
+    }
+    public static void main(String[] args) {
+        System.out.println(multiply());
+    }
+}
